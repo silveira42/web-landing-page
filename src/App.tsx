f59c22907d9ccdbf07167c46cd88cbf7220f16de
Bruno Silveira <silverdev42@gmail.com>
2024-02-08 20:47:58 -0300

    Changed home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,9 @@ function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
+				<h1>Under construction</h1>
 			</header>
+			<footer>thesilver®2024</footer>
 		</div>
 	);
 }
