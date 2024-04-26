d2ef2e400869dd6480a9298dca780edb7ff62d4a
Bruno Silveira <silverdev42@gmail.com>
2024-04-26 18:09:07 -0300

    Improved description

--- src/config/dictionaries/portuguese.ts
+++ src/config/dictionaries/portuguese.ts
@@ -1,6 +1,8 @@
 const portuguese = {
 	slogan: `Bem vindos ao Silver Hub. Essa página é a home do meu domínio,
-	e contém links para meus projetos públicos e também para meus apps pessoais.`,
+	e contém links para meus projetos públicos e também para meus apps pessoais.
+	Tais apps pessoais não devem funcionar para você, mas fique à vontade para
+	explorar os links públicos!`,
 	title: 'TheSilverHub',
 	publicLists: 'Links públicos',
 	personalApps: 'Apps pessoais',
